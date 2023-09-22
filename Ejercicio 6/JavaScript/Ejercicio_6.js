c2fd6db7c7ec90042a04703d98bb066484ff0226
FabrizioCM <142865932+FabrizioCM@users.noreply.github.com>
2023-09-22 14:44:15 -0600

    Add files via upload

---
+++ Ejercicio 6/JavaScript/Ejercicio_6.js
@@ -0,0 +1,4 @@
+//Estructura de lo que tengo que hacer
+let arregloMultidimensional = [1, [2, 3, [4, 5, [6]]]];
+let arregloArreglado = arregloMultidimensional.flat(Infinity);
+        console.log(`Tu arreglo final es: ${arregloArreglado}`);
